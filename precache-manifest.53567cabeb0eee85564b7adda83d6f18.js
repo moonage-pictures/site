66b422148ff6ff64fc465b8d442f23a4736ed139
Richard Tzanov <richard@jdlt.co.uk>
2018-12-17 20:04:43 +0000

    Updates

---
+++ precache-manifest.53567cabeb0eee85564b7adda83d6f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb5eadc7b933ad78e061",
+    "url": "/landing-page/static/css/main.5c2d32eb.chunk.css"
+  },
+  {
+    "revision": "fb5eadc7b933ad78e061",
+    "url": "/landing-page/static/js/main.fb5eadc7.chunk.js"
+  },
+  {
+    "revision": "f4bb04f8a4ae323e35e0",
+    "url": "/landing-page/static/js/1.f4bb04f8.chunk.js"
+  },
+  {
+    "revision": "5e502b1b7d9d8dee4025",
+    "url": "/landing-page/static/js/runtime~main.5e502b1b.js"
+  },
+  {
+    "revision": "e3d878cd140566af6511189f4c7effbd",
+    "url": "/landing-page/static/media/NeutraDisplay-Bold.e3d878cd.ttf"
+  },
+  {
+    "revision": "b78dfff6a102236597cb5827af7b9605",
+    "url": "/landing-page/index.html"
+  }
+];
