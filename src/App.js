fbfc1530414fb61a524917eecafb71cfc1f8b540
Richard Tzanov <richard@jdlt.co.uk>
2019-01-05 16:15:07 +0000

    add basename to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Contact from "./components/Contact";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Fragment>
         <Switch>
           <Route exact path="/" component={Home} />
