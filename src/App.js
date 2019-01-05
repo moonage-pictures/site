b82a220f8bcf2cfeddb7fcf1a2e9a317eb90d42d
Richard Tzanov <richard@jdlt.co.uk>
2019-01-05 11:41:03 +0000

    change css files to scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
 import "bulma/css/bulma.css";
 import Home from "./components/Home";
 import Navbar from "./components/common/Navbar";
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,35 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
-import "../../index.css";
 
 export default class Navbar extends Component {
-
   state = {
     navbarOpen: false
-  }
+  };
 
   toggleNavbar = () => {
     this.setState({ navbarOpen: !this.state.navbarOpen });
-  }
+  };
 
   componentDidUpdate(prevProps) {
-    if(prevProps.location.pathname !== this.props.location.pathname) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
       this.setState({ navbarOpen: false });
     }
   }
-
 
   render() {
     return (
       <nav className="navbar" role="navigation" aria-label="main-naivagation">
         <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <img src={logo} className="logo" alt="Moonage Pictures Logo" />
-          </Link>
-
           <a
-          className={`navbar-burger${this.state.navbarOpen ? ' is-active' : ''}`}
+            className={`navbar-burger${
+              this.state.navbarOpen ? " is-active" : ""
+            }`}
             role="button"
             href="#"
             aria-label="menu"
@@ -41,9 +36,20 @@ export default class Navbar extends Component {
             <span aria-hidden="true" />
           </a>
         </div>
-        <div className={`navbar-menu${this.state.navbarOpen ? ' is-active' : ''}`}>
+        <div
+          className={`navbar-menu${this.state.navbarOpen ? " is-active" : ""}`}
+        >
           <div className="navbar-end">
-            <Link to="/news" className="navbar-item">News</Link>
+            <Link to="/news" className="navbar-item">
+              News
+            </Link>
+            <Link to="/">
+              <img
+                src={logo}
+                className="navbar-logo navbar-item"
+                alt="Moonage Pictures Logo"
+              />
+            </Link>
           </div>
         </div>
       </nav>
