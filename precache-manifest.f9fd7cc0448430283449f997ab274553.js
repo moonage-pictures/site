2121471beccc10db0124a5d7e71f0fb61c4acbb1
Richard Tzanov <richardtzanov@gmail.com>
2019-02-13 19:41:22 +0000

    Updates

---
+++ precache-manifest.f9fd7cc0448430283449f997ab274553.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bda4e77ee6216cabd09be3d8948daa61",
+    "url": "/static/media/lulo-clean-one.bda4e77e.otf"
+  },
+  {
+    "revision": "a6bcd0cfa4c8e8695de530492c1ed566",
+    "url": "/static/media/logo.a6bcd0cf.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "835f9210a9920b606b78",
+    "url": "/static/js/main.835f9210.chunk.js"
+  },
+  {
+    "revision": "6abc23dbd5b494cd0969",
+    "url": "/static/js/1.6abc23db.chunk.js"
+  },
+  {
+    "revision": "835f9210a9920b606b78",
+    "url": "/static/css/main.edcfda42.chunk.css"
+  },
+  {
+    "revision": "6abc23dbd5b494cd0969",
+    "url": "/static/css/1.cf919d4d.chunk.css"
+  },
+  {
+    "revision": "4bf83b07feae4998d6d294bd83a36b63",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/landing-page/precache-manifest.6d74d0f6af9ee643a48276ca264c25d7.js"
+  "/precache-manifest.f9fd7cc0448430283449f997ab274553.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/landing-page/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
