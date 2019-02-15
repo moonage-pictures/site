f23dfed3811267c8fb462a627e4bf3e9150eb8d8
Richard Tzanov <richard@jdlt.co.uk>
2019-02-15 17:41:57 +0000

    Updates

---
+++ precache-manifest.3927fb89e3e89caba5857c296a79e505.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54aa80a9ee51b6a4a3f4",
+    "url": "/static/css/main.b576c10e.chunk.css"
+  },
+  {
+    "revision": "54aa80a9ee51b6a4a3f4",
+    "url": "/static/js/main.54aa80a9.chunk.js"
+  },
+  {
+    "revision": "8e598d40cb8d10f1a1fa",
+    "url": "/static/css/1.6eb198bf.chunk.css"
+  },
+  {
+    "revision": "8e598d40cb8d10f1a1fa",
+    "url": "/static/js/1.8e598d40.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6bcd0cfa4c8e8695de530492c1ed566",
+    "url": "/static/media/logo.a6bcd0cf.svg"
+  },
+  {
+    "revision": "bda4e77ee6216cabd09be3d8948daa61",
+    "url": "/static/media/lulo-clean-one.bda4e77e.otf"
+  },
+  {
+    "revision": "c56936e09af2407649580eb96a3105e9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d848d4b5485940550e1d9fa93f8e1a86.js"
+  "/precache-manifest.3927fb89e3e89caba5857c296a79e505.js"
 );
 
 workbox.clientsClaim();
