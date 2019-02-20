12d6bed5b01e5be6e233e33b441dd24328bac8d6
Richard Tzanov <richard@jdlt.co.uk>
2019-02-20 13:16:17 +0000

    add alligator img to Company

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -16,28 +16,50 @@ export default class Company extends Component {
       url: "https://cms.moonagepictures.com/wp-json/wp/v2/pages/2",
       method: "GET"
     });
-    this.setState({
-      body: data.content.rendered,
-      title: data.title.rendered,
-      loading: false
-    });
+    console.log("data is: ", data);
+    this.setState(
+      {
+        body: data.content.rendered,
+        title: data.title.rendered,
+        loading: false,
+        companyImage: data.acf.companyImage
+      },
+      () => console.log("state is: ", this.state)
+    );
   };
 
   render() {
-    const { loading, title, body } = this.state;
+    const { loading, title, body, companyImage } = this.state;
     return (
       <Fragment>
         <Navbar />
         <div className="container fade">
           <section className="section">
             {loading && <Loader />}
-            <div className="columns is-centered">
+            <div className="columns is-centered is-multiline">
               <div className="column is-two-thirds-desktop ">
                 <h1 className="title is-1 section-header">{title}</h1>
+
                 <div
                   className="page-content"
                   dangerouslySetInnerHTML={{ __html: body }}
                 />
+              </div>
+              <div
+                className="column is-two-thirds-desktop is-centered"
+                style={{
+                  display: "flex",
+                  justifyContent: "center"
+                }}
+              >
+                <figure
+                  className="image"
+                  style={{ width: "50%", height: "50%" }}
+                >
+                  {companyImage && (
+                    <img src={companyImage.url} alt="Moonage Alligator" />
+                  )}
+                </figure>
               </div>
             </div>
           </section>
