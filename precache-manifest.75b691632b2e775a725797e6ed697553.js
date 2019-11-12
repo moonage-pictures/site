dc3081d4e9aa3ebdb61f82f34cbd4d4c3510b6ef
Richard Tzanov <richard@jdlt.co.uk>
2019-11-12 09:29:23 +0000

    Updates

---
+++ precache-manifest.75b691632b2e775a725797e6ed697553.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0adeea0e0edf11c8c419b0aa7c978f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b72e58f25c941d2027",
+    "url": "/static/css/2.3e57c46c.chunk.css"
+  },
+  {
+    "revision": "8ffc22c1a1448e202d29",
+    "url": "/static/css/main.569f39e4.chunk.css"
+  },
+  {
+    "revision": "85b72e58f25c941d2027",
+    "url": "/static/js/2.43d8e921.chunk.js"
+  },
+  {
+    "revision": "8ffc22c1a1448e202d29",
+    "url": "/static/js/main.29c88044.chunk.js"
+  },
+  {
+    "revision": "3bae116586e425010f09",
+    "url": "/static/js/runtime-main.840ac8e0.js"
+  },
+  {
+    "revision": "a6bcd0cfa4c8e8695de530492c1ed566",
+    "url": "/static/media/logo.a6bcd0cf.svg"
+  },
+  {
+    "revision": "bda4e77ee6216cabd09be3d8948daa61",
+    "url": "/static/media/lulo-clean-one.bda4e77e.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39369a8378c09149a54a49983f8d1a65.js"
+  "/precache-manifest.75b691632b2e775a725797e6ed697553.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
